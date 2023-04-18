9fd815e6690982720849e80779e17955b445baa8
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-04-18 23:26:07 +0800

    Update string.py

--- string.py
+++ string.py
@@ -1,15 +1,18 @@
-#滚动哈希：
-# 字符串编码函数
-def ecd(ch):
-    return ord(ch) - ord('a') + 1
-
-mod = 10**9+7
-base = 31       # s仅包含小写字母，base可取31
-
-n = len(s)
-# 预处理出前缀字符串的哈希值 和 base进制的幂
-prefix = [0] * (n+1)    # prefix[i]: 字符串s[0:i]的哈希值
-mul = [1] * (n+1)       # mul[i]: base ** i
-for i in range(1, n+1):
-    prefix[i] = ( prefix[i-1] * base + ecd(s[i-1]) ) % mod
-    mul[i] = mul[i-1] * base % mod
+#滚动哈希，例题：1044. 最长重复子串。
+n = len(s) # s 要处理的字符串
+MOD = 10**9 + 7
+base1, base2 = 31, 71   # 双哈希：两套进制
+# encoder：获取字母在字母表中的顺序: 
+ecd = {chr(i+ ord('a')):i for i in range(26)}     
+hvs1, hvs2 = [0] * (n+1), [0] * (n+1)
+pow1, pow2 = [1] * (n+1), [1] * (n+1)
+for i in range(n):
+hvs1[i+1] = (hvs1[i] * base1 + ecd[s[i]]) % MOD
+hvs2[i+1] = (hvs2[i] * base2 + ecd[s[i]]) % MOD
+pow1[i+1] = (pow1[i] * base1) % MOD
+pow2[i+1] = (pow2[i] * base2) % MOD
+# 获取子串哈希值，元组形式返回:
+def get_hash(start, end):
+    hash_1 = (hvs1[end] - hvs1[start] * pow1[end-start]) % MOD
+    hash_2 = (hvs2[end] - hvs2[start] * pow2[end-start]) % MOD
+    return (hash_1, hash_2)
