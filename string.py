65fc4e0ff50718c07ed9bd1ef26c253cc5bfad39
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-04-18 23:34:57 +0800

    Update string.py

--- string.py
+++ string.py
@@ -7,10 +7,10 @@ ecd = {chr(i+ ord('a')):i for i in range(26)}
 hvs1, hvs2 = [0] * (n+1), [0] * (n+1)
 pow1, pow2 = [1] * (n+1), [1] * (n+1)
 for i in range(n):
-hvs1[i+1] = (hvs1[i] * base1 + ecd[s[i]]) % MOD
-hvs2[i+1] = (hvs2[i] * base2 + ecd[s[i]]) % MOD
-pow1[i+1] = (pow1[i] * base1) % MOD
-pow2[i+1] = (pow2[i] * base2) % MOD
+    hvs1[i+1] = (hvs1[i] * base1 + ecd[s[i]]) % MOD
+    hvs2[i+1] = (hvs2[i] * base2 + ecd[s[i]]) % MOD
+    pow1[i+1] = (pow1[i] * base1) % MOD
+    pow2[i+1] = (pow2[i] * base2) % MOD
 # 获取子串s[start:end]哈希值，元组形式返回:
 def get_hash(start, end):
     hash_1 = (hvs1[end] - hvs1[start] * pow1[end-start]) % MOD
