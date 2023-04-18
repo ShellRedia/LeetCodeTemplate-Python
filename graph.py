5f6cda8a9ce58f6ed7cbf98640449e9aa9b5b2e0
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-04-18 16:34:17 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -9,7 +9,8 @@ def merge(a, b):
     if x == y:
         return
     p[x] = y
-p = list(range(10 ** 5 + 1))
+N = 10 ** 5 + 1
+p = list(range(N))
 
 # 广度优先搜索:
 m, n = len(grid), len(grid[0])
@@ -24,3 +25,33 @@ while dq:
         if 0 <= nx < m and 0 <= ny < n and (nx, ny) not in vis and cond:
             dq.append((nx, ny))
             vis.add((nx, ny))
+            
+# 深度优先搜索: TODO
+
+# 最短路径 
+# Dijkstra: -- 单源无负权 -- :
+g = defaultdict(list)
+        
+for s, t, v in edges:
+    g[s].append((t,v+1))
+    # g[t].append((s,v+1)) # 无向图
+
+def get_dist(src):
+    dist=[inf]*n
+    dist[src]=0   
+    ss = SortedSet()
+
+    ss.add((0, src))
+    while ss:
+        d, x = ss[0]
+        ss.remove((d,x))
+        if d > dist[x] or dist[x] == inf:
+            continue
+        for y, cost in table[x]:
+            if dist[x] + cost < dist[y]:
+                dist[y] = dist[x] + cost
+                ss.add((dist[y], y))
+    return dist
+
+src = 0            
+dist = get_dist(src) # src 到各个点的距离，保存为数组形式
