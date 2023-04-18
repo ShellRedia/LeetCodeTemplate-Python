3af24930100277cde4659f4b7ff22fd8fe6d072f
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-04-18 16:03:17 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -10,3 +10,16 @@ def merge(a, b):
         return
     p[x] = y
 p = list(range(10 ** 5 + 1))
+
+# 广度优先搜索:
+m, n = len(grid), len(grid[0])
+dq = deque()
+vis = set()
+while dq:
+    x, y = dq[0]
+    dq.popleft()
+    for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1 and (nx, ny) not in vis:
+            dq.append((nx, ny))
+            vis.add((nx, ny))
