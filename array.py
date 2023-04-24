a643740bccc1446bacf35d82e14732585703860e
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-04-24 11:10:32 +0800

    Update array.py

--- array.py
+++ array.py
@@ -20,3 +20,12 @@ def permuteUnique(nums):
     # 记得给nums排序
     backtracking(sorted(nums),used,[])
     return res
+
+# 循环码(返回二进制字符串列表，参数n为二进制长度):
+def cyclic_code(n):
+    if n < 0:
+        return []
+    codes = ['0', '1']
+    for i in range(n - 1):
+        codes = [code + '0' for code in codes] + [code + '1' for code in reversed(codes)]
+    return codes
