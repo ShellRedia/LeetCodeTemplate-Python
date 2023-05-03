fb95818651f2b9b38f329aa89b24b9c5e652f96b
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-05-03 10:30:40 +0800

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -6,14 +6,13 @@
 # 选词填空，这是最近才遇到的问题(2023年5月3日)，大致意思是有一个串里面有若干空，
 # 每个空有若干个候选项，问有多少种填空方式，自己写也不难，但是总感觉花时间。
 # 形式：AB[CDE]FG[HI] -> [ABCFGH, ABCFGI, ABDFGH, ABDFGI, ABEFGH, ABEFGI]
-# 参数：idx: 当前正在填写的索引
-# 临时变量：rt: 表示当前正在搜索的路径，perm: 存放所有符合条件的路径
-rt, perm = [], []
 template = ["A", "B", "_", "F", "G", "_"] # 模板串
-fill_positions = [0, 2] # 要填写选项位置
+fill_positions = [2, 5] # 要填写选项位置
 options = [["C", "D", "E"], ["H", "I"]] # 可供填写的选项（离散化处理）
 get_fill_pos = {x:i for i, x in enumerate(fill_positions)} # 解离散化
-def permutation_options(idx):
+# 临时变量：rt: 表示当前正在搜索的路径，perm: 存放所有符合条件的路径
+rt, perm = [], []
+def permutation_options(idx): # 参数：idx: 当前正在填写的索引
     if idx == len(template):
         perm.append(rt.copy())
         return 
