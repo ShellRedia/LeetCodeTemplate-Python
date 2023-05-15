93ecd65249f2771d00bd64d7c18e42cf5fcd765d
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-05-15 22:32:25 +0800

    Update array.py

--- array.py
+++ array.py
@@ -21,6 +21,23 @@ def permuteUnique(nums):
     backtracking(sorted(nums),used,[])
     return res
 
+# 二维差分:快速地把一个矩形范围内的数都 + x
+m, n = len(grid), len(grid[0])
+diff = [[0] * (m + 2) for _ in range(n + 2)]
+for r1, r2, c1, c2, x in rects:
+    # 将区域 r1<=r<=r2 && c1<=c<=c2 上的数都加上 x
+    # 多 +1 是为了方便求后面用二维前缀和复原
+    diff[r1 + 1][c1 + 1] += x
+    diff[r1 + 1][c2 + 2] -= x
+    diff[r2 + 2][c1 + 1] -= x
+    diff[r2 + 2][c2 + 2] += x
+# 根据差分矩阵复原，得到矩阵的真实值:
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        diff[i][j] += diff[i][j - 1] + diff[i - 1][j] - diff[i - 1][j - 1]
+mat = diff
+    
+
 # 最长递增子序列(LIS)
 from sortedcontainers import *
 def lengthOfLIS(nums: List[int]) -> int:
