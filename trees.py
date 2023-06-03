cc156f753bd705d3ea73be35ab9fbf74a8265b26
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-06-03 20:17:09 +0800

    update

--- trees.py
+++ trees.py
@@ -1,3 +1,45 @@
+# 前缀树, 注意要灵活修改，否则也可能超时:
+# insert: 插入一个串; search_count: 指定串出现的次数; pre_count: 指定前缀出现的次数
+class TrieNode:
+    def __init__(self):
+        self.cnt = 0
+        self.children = defaultdict(TrieNode)
+
+class Trie:
+    def __init__(self):
+        self.head = TrieNode()
+
+    def insert(self, arr) -> None:
+        cur = self.head
+        for x in arr:
+            if not cur.children[x]:
+                cur.children[x] = TrieNode(x)
+            cur = cur.children[x]
+            cur.cnt += 1
+
+    def search_count(self, arr) -> int:
+        cur = self.head
+        rnt = 0
+        for x in arr:
+            if not cur.children[x]:
+                return False
+            cur = cur.children[x]
+            rnt = cur.cnt
+        for x in cur.children.values():
+            rnt -= x.cnt
+        return rnt
+
+    def pre_count(self, prefix) -> int:
+        cur = self.head
+        rnt = 0
+        for x in prefix:
+            if not cur.children[x]:
+                return 0
+            cur = cur.children[x]
+            rnt = cur.cnt
+        return rnt
+trie = Trie()
+
 # 抄的板子，初始索引为0， 查询修改的区间范围为[L, R]的闭区间
 class LazySegmentTree:
     def __init__(self, nums: List[int]):
