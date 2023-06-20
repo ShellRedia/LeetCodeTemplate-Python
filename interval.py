719ad601da9dd6c67127d458941d4dddfd286630
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-06-20 09:24:50 +0800

    update

--- interval.py
+++ interval.py
@@ -1,26 +1,26 @@
 # 区间问题，对于区间问题总有些共性可以总结:
-# 合并
+# 并集
 def merge(intervals: List[List[int]]) -> List[List[int]]:
     if not intervals: return []
     intervals.sort()
-    merged_intervals = [intervals[0]]
+    merged = [intervals[0]]
     for x, y in intervals[1:]:
-        if merged_intervals[-1][1] < x:
-            merged_intervals.append([x, y])
+        if merged[-1][1] < x:
+            merged.append([x, y])
         else:
-            merged_intervals[-1][1] = max(y, merged_intervals[-1][1])
-    return merged_intervals
+            merged[-1][1] = max(y, merged[-1][1])
+    return merged
 
-# 求交集
-def intersection(A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
-    ans = []
-    i = j = 0
-    while i < len(A) and j < len(B):
-        lo = max(A[i][0], B[j][0])
-        hi = min(A[i][1], B[j][1])
-        if lo <= hi:
-            ans.append([lo, hi])
-        if A[i][1] < B[j][1]: i += 1
-        else: j += 1
-    return ans
+# 交集
+def intersection(intervals: List[List[int]]) -> List[List[int]]:
+    if not intervals: return []
+    intervals.sort()
+    intersection = []
+    px, py = intervals[0]
+    for x, y in intervals[1:]:
+        if py >= x:
+            intersection.append([max(px, x), min(py, y)])
+        if y > py:
+            px, py = x, y
+    return intersection
 
