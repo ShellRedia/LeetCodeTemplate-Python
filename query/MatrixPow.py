67d25826dad3bd942fd8c9a95f774dfc29b9cd15
ShellRedia <34744491+ShellRedia@users.noreply.github.com>
2023-09-18 20:00:58 +0800

    update

--- query/MatrixPow.py
+++ query/MatrixPow.py
@@ -1,20 +1,39 @@
+'''
+k 个 同样的方阵相乘
+'''
 class MatrixPow:
-    def __init__(self):
+    def __init__(self, matrix):
+        self.matrix = matrix
+        self.n = len(matrix)
         self.MOD = 10 ** 9 + 7
+
     # 矩阵乘法
     def multiply(self, a: List[List[int]], b: List[List[int]]) -> List[List[int]]:
-        c = [[0, 0], [0, 0]]
-        for i in range(2):
-            for j in range(2):
-                c[i][j] = (a[i][0] * b[0][j] + a[i][1] * b[1][j]) % self.MOD
+        c = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        for i in range(self.n):
+            for j in range(self.n):
+                for k in range(self.n):
+                    c[i][j] += a[i][k] * b[k][j] 
+                    #c[i][j] %= self.MOD
         return c
 
     # 矩阵快速幂
-    def pow(self, a: List[List[int]], n: int) -> List[List[int]]:
-        res = [[1, 0], [0, 1]]
-        while n:
-            if n % 2:
+    def pow(self, k: int) -> List[List[int]]:
+        res = [[0 for _ in range(self.n)] for _ in range(self.n)]
+        a = self.matrix
+        for i in range(self.n): res[i][i] = 1
+        while k:
+            if k % 2:
                 res = self.multiply(res, a)
             a = self.multiply(a, a)
-            n //= 2
+            k //= 2
         return res
+
+# if __name__=="__main__":
+#         m = [
+#             [1, 1],
+#             [1, 0]
+#         ]
+#         mp = MatrixPow(m)
+#         m = mp.pow(k)
+#         return m[0][1]
